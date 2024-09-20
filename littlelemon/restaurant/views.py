15eeb57cde2d2381ff876c210c76c2f9dd7994e3
JunSung Jeon <jjs980627@gmail.com>
2024-09-21 00:24:55 +0900

    Set up Djoser and Token Auth

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics, viewsets
-
 from .models import Menu, Booking
-
 from .serializers import MenuSerializer, BookingSerializer
+
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -25,3 +25,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
